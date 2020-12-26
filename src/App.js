d1003f7f54658683fbff4b789267ec3fe59ccda6
Favour Afolayan <favourafolayan@gmail.com>
2020-12-26 05:09:56 +0100

    changed the items to nba clubs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 import "./styles.css";
 
-import alphabets from './alphabets';
+import store from "./store";
 
 export default function App() {
-  const [items, setItems] = React.useState(alphabets);
+  const [items, setItems] = React.useState(store);
 
   const moveItem = (index) => {
     if (index === 0) return;
@@ -20,41 +20,56 @@ export default function App() {
     setItems(tempArray);
   };
 
-const handleDrag = (event, indexOfItem, itemDragged) => {
-  event.dataTransfer.dropEffect = 'move';
-  event.dataTransfer.effectAllowed = 'move';
-  event.dataTransfer.setData('index-dragged', indexOfItem);
-  event.dataTransfer.setData('item-dragged', itemDragged);
-}
+  const handleDrag = (event, indexOfItem, itemDragged) => {
+    event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("index-dragged", indexOfItem);
+    event.dataTransfer.setData("item-dragged", JSON.stringify(itemDragged));
+    event.dataTransfer.setData("original-element", event.target);
+  };
 
-const handleDrop = (indexOfItemDroppedOn, event, dropValue) => {
-  const indexOfDraggedItem = event.dataTransfer.getData('index-dragged');
-  const draggedItem = event.dataTransfer.getData('item-dragged');    
-  const tempArray = [...items];
-    
-  tempArray.splice(indexOfItemDroppedOn, 1, draggedItem);
-  tempArray.splice(indexOfDraggedItem, 1, dropValue);
+  const handleDrop = (indexOfItemDroppedOn, event, dropValue) => {
+    const indexOfDraggedItem = event.dataTransfer.getData("index-dragged");
+    const draggedItem = JSON.parse(event.dataTransfer.getData("item-dragged"));
+    const originalElement = event.dataTransfer.getData("original-element");
 
-  setItems(tempArray);
-}
+    const tempArray = [...items];
+
+    tempArray.splice(indexOfItemDroppedOn, 1, draggedItem);
+    tempArray.splice(indexOfDraggedItem, 1, dropValue);
+
+    console.log(originalElement, "orignal");
+
+    // originalElement.classList.remove("dragged");
+    setItems(tempArray);
+  };
+
+  const preventDefault = (event) => event.preventDefault();
 
   return (
     <div className="App">
-      <h2>Click or drag, your choice!!!</h2>
+      <h2 className="heading">Click or drag, your choice!!!</h2>
 
       <div className="items">
         {items.map((item, index) => (
-          <p 
-            key={index} 
-            onDragOver={event => event.preventDefault()} 
-            onDragEnter={event => event.preventDefault()} 
-            onDrop={event => handleDrop(index, event, item)} 
-            onDragStart={event => handleDrag(event, index, item)} 
-            draggable="true" 
+          <div
+            key={index}
+            className="item"
+            onDragOver={preventDefault}
+            onDragEnter={preventDefault}
+            onDrop={(event) => handleDrop(index, event, item)}
+            onDragStart={(event) => handleDrag(event, index, item)}
+            draggable="true"
             onClick={() => moveItem(index)}
-            >
-            {item}
-          </p>
+          >
+            <img
+              src={item.image}
+              draggable="false"
+              alt="property"
+              className="item-image"
+            />
+            <p className="item-name">{item.name}</p>
+          </div>
         ))}
       </div>
     </div>
