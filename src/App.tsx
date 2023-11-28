cad31c4f2e5cac2f5048f6efa27810c5457b327f
Favour Afolayan <favourafolayan@gmail.com>
2023-11-28 18:04:05 +0100

    Restart app with vite

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import _shuffle from 'lodash.shuffle';
+
+import './index.css';
+
+import store from './store';
+
+export default function App() {
+  const [items, setItems] = useState(store);
+
+  const moveItem = (index: number) => {
+    if (index === 0) return;
+    const tempArray = [...items];
+    const newPosition = index - 1;
+
+    const itemToBeMovedUp = tempArray[index];
+    const itemToBeMovedDown = tempArray[newPosition];
+
+    tempArray.splice(index, 1, itemToBeMovedDown);
+    tempArray.splice(newPosition, 1, itemToBeMovedUp);
+    setItems(tempArray);
+  };
+
+  const handleDrag = (event, indexOfItem, itemDragged) => {
+    event.target.classList.add('dragged');
+    event.dataTransfer.dropEffect = 'move';
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('index-dragged', indexOfItem);
+    event.dataTransfer.setData('item-dragged', JSON.stringify(itemDragged));
+    event.dataTransfer.setData('original-element', event.target);
+    event.target.classList.remove('dragged');
+  };
+
+  const handleTouchStart = (event, indexOfItem, itemDragged) => {
+    debugger;
+    // event.dataTransfer.dropEffect = 'move';
+    // event.dataTransfer.effectAllowed = 'move';
+    // event.dataTransfer.setData('index-dragged', indexOfItem);
+    // event.dataTransfer.setData('item-dragged', JSON.stringify(itemDragged));
+    // event.dataTransfer.setData('original-element', event.target);
+    // event.target.classList.remove('dragged');
+
+    // const tempArray = [...items];
+    // tempArray.splice(indexOfItem, 1);
+  };
+
+  const handleDrop = (indexOfItemDroppedOn, event, dropValue) => {
+    const indexOfDraggedItem = event.dataTransfer.getData('index-dragged');
+    const draggedItem = JSON.parse(event.dataTransfer.getData('item-dragged'));
+
+    const tempArray = [...items];
+
+    tempArray.splice(indexOfItemDroppedOn, 1, draggedItem);
+    tempArray.splice(indexOfDraggedItem, 1, dropValue);
+
+    setItems(tempArray);
+  };
+
+  const preventDefault = (
+    event: React.DragEvent<HTMLDivElement> | undefined
+  ) => {
+    event?.preventDefault();
+  };
+
+  const reset = () => setItems(store);
+
+  const shuffle = () => {
+    const shuffledItems = _shuffle(items);
+    setItems(shuffledItems);
+  };
+
+  return (
+    <div className="App">
+      <h2 className="heading">Click or drag, your choice!!!</h2>
+
+      <div className="controls">
+        <button className="button shuffle-button" onClick={shuffle}>
+          Shuffle
+        </button>
+        <button className="button reset-button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+
+      <div className="items">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="item"
+            onDragOver={preventDefault}
+            onDragEnter={preventDefault}
+            onDrop={(event) => handleDrop(index, event, item)}
+            onTouchEnd={(event) => handleDrop(index, event, item)}
+            onDragStart={(event) => handleDrag(event, index, item)}
+            onTouchStart={(event) => handleTouchStart(event, index, item)}
+            draggable="true"
+            onClick={() => moveItem(index)}
+          >
+            <img
+              src={item.image}
+              draggable="false"
+              alt="property"
+              className="item-image"
+            />
+            <p className="item-name">{item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/store.ts
@@ -0,0 +1,134 @@
+const store = [
+  {
+    image: 'https://content.sportslogos.net/logos/6/220/thumbs/22091682016.gif',
+    name: 'Atlanta Hawks',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/213/thumbs/slhg02hbef3j1ov4lsnwyol5o.gif',
+    name: 'Boston Celtics',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/3786/thumbs/hsuff5m3dgiv20kovde422r1f.gif',
+    name: 'Brooklyn Nets',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/5120/thumbs/512019262015.gif',
+    name: 'Charlotte Hornets',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/221/thumbs/hj3gmh82w9hffmeh3fjm5h874.gif',
+    name: 'Chicago Bulls',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/222/thumbs/22269212018.gif',
+    name: 'Cleveland Cavaliers',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/228/thumbs/22834632018.gif',
+    name: 'Dallas Mavericks',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/229/thumbs/22989262019.gif',
+    name: 'Denver Nuggets',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/223/thumbs/22321642018.gif',
+    name: 'Detroit Pistons',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/235/thumbs/23531522020.gif',
+    name: 'Golden State Warriors',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/230/thumbs/23068302020.gif',
+    name: 'Houston Rockets',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/224/thumbs/22448122018.gif',
+    name: 'Indiana Pacers',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/236/thumbs/23654622016.gif',
+    name: 'LA Clippers',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/237/thumbs/uig7aiht8jnpl1szbi57zzlsh.gif',
+    name: 'LA Lakers',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/231/thumbs/23143732019.gif',
+    name: 'Memphis Grizzlies',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/214/thumbs/burm5gh2wvjti3xhei5h16k8e.gif',
+    name: 'Miami Heat',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/225/thumbs/22582752016.gif',
+    name: 'Milwaukee Bucks',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/232/thumbs/23296692018.gif',
+    name: 'Minnesota Timberwolves',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/4962/thumbs/496226812014.gif',
+    name: 'New Orleans Pelicans',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/216/thumbs/2nn48xofg0hms8k326cqdmuis.gif',
+    name: 'New York Knicks',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/2687/thumbs/khmovcnezy06c3nm05ccn0oj2.gif',
+    name: 'Oklahoma City Thunder',
+  },
+  {
+    image:
+      'https://content.sportslogos.net/logos/6/217/thumbs/wd9ic7qafgfb0yxs7tem7n5g4.gif',
+    name: 'Orlando Magic',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/218/thumbs/21870342016.gif',
+    name: 'Philadephia 76ers',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/238/thumbs/23843702014.gif',
+    name: 'Phoenix Suns',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/239/thumbs/23997252018.gif',
+    name: 'Portland Blazers',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/240/thumbs/24040432017.gif',
+    name: 'Sacramento Kings',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/233/thumbs/23325472018.gif',
+    name: 'San Antonio Spurs',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/227/thumbs/22770242021.gif',
+    name: 'Toronto Raptors',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/234/thumbs/23467492017.gif',
+    name: 'Utah Jazz',
+  },
+  {
+    image: 'https://content.sportslogos.net/logos/6/219/thumbs/21956712016.gif',
+    name: 'Washington Wizards',
+  },
+];
+
+export default store;
